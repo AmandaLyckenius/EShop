fac436d0e7b6b9af5e762a94cc8cb08b5e2cfdae
Floremila <flormequi@gmail.com>
2025-05-06 10:11:30 +0200

    Add: Console message for add/remove products

--- src/main/java/com/EShop/Customer.java
+++ src/main/java/com/EShop/Customer.java
@@ -1,7 +1,38 @@
 package com.EShop;
 
 public class Customer {
-    private String username;
-    private String password;
+    private String name;
+    private double saldo;
     private Cart cart;
+
+    public Customer(String name, double saldo, Cart cart) {
+        this.name = name;
+        this.saldo = saldo;
+        this.cart = cart;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
 }
