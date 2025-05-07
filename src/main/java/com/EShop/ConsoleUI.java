130638cbc61d1175f35b0d554e12ed6be10dcd70
Floremila <flormequi@gmail.com>
2025-05-07 13:20:03 +0200

    Refactor: instansiation from ConsoleUI to main and add ScanerInt method.

--- src/main/java/com/EShop/ConsoleUI.java
+++ src/main/java/com/EShop/ConsoleUI.java
@@ -10,18 +10,13 @@ import java.util.Scanner;
 public class ConsoleUI {
     private boolean running = true;
     private final Scanner scanner = new Scanner(System.in);
-    Discount discount10 = new TenPercent();
-    Discount discount20 = new TwentyPercent();
-    Product products = new Product();
-    List<Product> productList = new ArrayList<>();
-    Cart cart = new Cart(productList);
-    List <Product> allProducts = getAllProducts();
+
 
     public String getWelcomeMessage() {
         return "Welcome to STI Bakery";
     }
 
-    public void printWelcomeMenu() {
+    public void printWelcomeMenu(Product products, List<Product> allProducts) {
         System.out.println(getWelcomeMessage());
         products.showProducts(allProducts);
         System.out.println(options());
@@ -30,13 +25,17 @@ public class ConsoleUI {
 
     private void getUserInput() {
         while (running) {
-            String userInput = createScanner().toLowerCase();
+            String userInput = createScannerString();
             processInput(userInput);
         }
     }
 
-    String createScanner() {
-        return scanner.nextLine();
+    int createScannerInt() {
+        return scanner.nextInt();
+    }
+
+    String createScannerString() {
+        return scanner.next();
     }
 
     public boolean processInput(String input) {
@@ -44,21 +43,6 @@ public class ConsoleUI {
     }
 
 
-
-
-
-    public static List<Product> getAllProducts() {
-        return new ArrayList<>(List.of(
-                new Product(1,"Chocolate Chip Cookie", "One chocolate chip cookie", "Cookies", 25.00, 1),
-                new Product(2,"Chocolate Cupcake", "Chocolate cupcake with cocoa nibs and sprinkles", "Cupcakes", 52.00, 1),
-                new Product(3,"Red Velvet Cupcake", "Red velvet cupcake with white cream frosting and red sprinkles", "Cupcakes", 52.00, 1),
-                new Product(4,"Lemon Cupcake", "Fresh lemon cupcake with white frosting and yellow lemon zest topping", "Cupcakes", 52.00, 1),
-                new Product(5,"Chocolate Cake", "8 piece cake with chocolate flavour", "Cakes", 475.00, 1),
-                new Product(6,"Macaron Cake", "12 piece vanilla flavoured cake with decorative macarons", "Cakes", 625.00, 1),
-                new Product(7,"Macaron Box Mix Small", "A box containing five macarons with different flavours", "Macarons", 60.00, 1),
-                new Product(8,"Macaron Box Mix Big", "A box containing 16 macarons with different flavours", "Macarons", 225.00, 1)
-        ));
-    }
 
     public String options() {
         return "What do you want to do next? \n" +
