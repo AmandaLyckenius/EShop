777c5fb978f7c17420f10fc1c345833cefd5d4ae
amandalyckenius <alyckenius@gmail.com>
2025-05-08 16:44:57 +0200

    Test: passing tests after implementing logic in createCartItem method

--- src/main/java/com/EShop/CartItemCreator.java
+++ src/main/java/com/EShop/CartItemCreator.java
@@ -7,6 +7,16 @@ public class CartItemCreator {
         this.productService = productService;
     }
 
+    public CartItem createCartItem(int articleNumber, int quantity) {
+        Product result = productService.findByArticleNumber(articleNumber);
 
+        if (result != null && quantity>0) {
+            CartItem cartItem = new CartItem(result,quantity);
+            return cartItem;
+        }
+
+        return null;
+
+    }
 
 }
